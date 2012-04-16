f2400ad0f37cfde9fc62b4179da7b4228b9773c1
lowrey.jeremy@gmail.com <lowrey.jeremy@gmail.com@78e05d51-7f9b-cf76-7049-e436a2377101>
2012-04-16 18:06:28 +0000

    client server working need seperate java project

--- G1MoonPie/src/g1/MoonPie/MoonPieActivity.java
+++ G1MoonPie/src/g1/MoonPie/MoonPieActivity.java
@@ -1,6 +1,7 @@
 package g1.MoonPie;
 
 import g1.MoonPie.Model.Edge;
+import g1.MoonPie.clientServer.ClientLauncher;
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -10,12 +11,16 @@ public class MoonPieActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
+        System.out.println("above try");
         try {
-			//ClientLauncher client = new ClientLauncher();
+        	System.out.println("before client");
+			ClientLauncher client = new ClientLauncher();
+			System.out.println("after client");
 		} catch (Exception e) {
 			System.out.println("FAILED");
 			e.printStackTrace();
 		}
+        System.out.println("below try");
         setContentView(R.layout.welcome);
         
     }
