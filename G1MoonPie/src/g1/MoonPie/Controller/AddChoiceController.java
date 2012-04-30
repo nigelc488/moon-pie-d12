063991bd0be3f6f2e426ccfc53efc7f30dee61a6
lowrey.jeremy@gmail.com <lowrey.jeremy@gmail.com@78e05d51-7f9b-cf76-7049-e436a2377101>
2012-04-30 20:31:54 +0000

    Added java doc and cleaned AddChoiceController

--- G1MoonPie/src/g1/MoonPie/Controller/AddChoiceController.java
+++ G1MoonPie/src/g1/MoonPie/Controller/AddChoiceController.java
@@ -18,7 +18,7 @@ import g1.MoonPie.Model.User;
 import g1.MoonPie.View.CloseEventView;
 import g1.MoonPie.clientServer.sendMessages.SendMessageController;
 
-/**This class is used called when attempting to add choices to an event.
+/**This class is called when attempting to add choices to an event.
  * 
  * @author J Lowrey
  * @author Eric Cobane
@@ -27,15 +27,29 @@ import g1.MoonPie.clientServer.sendMessages.SendMessageController;
  */
 public class AddChoiceController implements android.view.View.OnClickListener{
 
+	/** The event. */
 	Event event;
+
+	/** The activity. */
 	Activity activity;
+
+	/** The lines. */
 	Line[] lines;
+
+	/** The names valid */
 	boolean namesValid = true;
+
+	/**  An array of all the textBoxes gathered from the choiceform layout */
 	EditText[] textBoxes = new EditText[8];
+
+	/** An array of all the Labels gathered from the choiceform layout */
 	TextView[] labels = new TextView[8];
+
+	/** The choices in an array. */
 	String[] choices;
 
 	/**This constructor creates an AddChoiceController with an event and an activity.
+	 * It also sets all of the android widgets; TextView and EditText as variables in the class
 	 * 
 	 * @param e	Event to access model objects.
 	 * @param a	Activity to access GUI objects.
@@ -81,34 +95,28 @@ public class AddChoiceController implements android.view.View.OnClickListener{
 		labels[6] = label7;
 		labels[7] = label8;
 
-
-//	NOT NEEDED ANYMORE
-//		for (int i = 0; i < textBoxes.length; i++){
-//
-//
-//			/** Visibility
-//			 * Visible = 0
-//			 * inVisible = 4
-//			 * GONE, invisible and doesn't take up any space 8
-//			 */
-//			textBoxes[i].setVisibility(4);
-//			labels[i].setVisibility(4);
-//		}
-
 	}
 
-	
+
+	/**
+	 * onClick creates a send message createRequest from SendMessageController
+	 * only if the names are valid it will check to see if you are a moderator if you are then set the choices based on if it is open or close
+	 * open will set only the first choice, closed will set all
+	 * 
+	 * @param v the current view where the click is taking place
+	 * 
+	 * @see android.view.View.OnClickListener#onClick(android.view.View)
+	 */
 	@Override
 	public void onClick(View v) {
+
 		namesValid = true;
 		lines = Event.getInstance().getLines();
 		String choice;
-		
-		//get and validate choices
 		choices = getChoices();
-		
+
 		if(namesValid){
-			//if moderator then sent create request
+			//if moderator then send create request
 			if(Event.getInstance().getUser().getPostion() == 0){
 				SendMessageController.createRequest(Event.getInstance().getIsOpen(), Event.getInstance().getQuestion(), Event.getInstance().getNumChoices(), Event.getInstance().getNumRounds(), Event.getInstance().getUser().getUsername(), Event.getInstance().getUser().getPassword(), choices);
 				if (!Event.getInstance().getIsOpen()){
@@ -132,90 +140,28 @@ public class AddChoiceController implements android.view.View.OnClickListener{
 			if (event.getIsOpen()) new CloseEventView(event, activity);
 		}
 	}
-		
-		
-//		/* if the event is open */
-//		if (event.getIsOpen()){
-//			
-//			
-//			
-//			//THIS IS ACTUALLY STILL WRONG
-//			for(int i=0; i<lines.length; i++){
-//				if(lines[i].getChoice().equals("")){
-//					lines[i].setChoice(textBoxes[0].getText().toString());
-//					
-//					//if you are the moderator then need to send create message
-//					if(Event.getInstance().getUser().getPostion() == 0){
-//						SendMessageController.createRequest(Event.getInstance().getIsOpen(), Event.getInstance().getQuestion(), Event.getInstance().getNumChoices(), Event.getInstance().getNumRounds(), Event.getInstance().getUser().getUsername(), Event.getInstance().getUser().getPassword());
-//					}
-//					//if you are not moderator then send add choice message
-//					else{
-//						SendMessageController.addChoiceRequest(Event.getInstance().getID(), i, lines[i].getChoice());
-//					}	
-//				}
-//			}
-//		//	User mod = new User("username", "password", true);					//This was used to test close event controller
-//		//	CloseEventView view = new CloseEventView(event, mod, activity);  	//This was used to test close event controller
-//		}
-//
-//		/*If the event is closed*/
-//		else{
-//			ArrayList<String> namesOfLines = new ArrayList<String>();
-//			for (int i = 0; i < lines.length; i++){
-//				String namedLine = textBoxes[i].getText().toString();
-//				namesOfLines.add(namedLine);
-//			}
-//			
-////			SendMessageController.createRequest(type, question, numChoices, numRounds, userName, password, event);
-//			
-//			
-////			SendMessageController.createRequest(Event., question, numChoices, numRounds, userName, password, event)
-//
-//			namesValid = true;
-//			if (namesOfLines.contains("")){
-//				Toast.makeText(activity, "Please fill in empty boxes", Toast.LENGTH_SHORT).show();
-//				namesValid = false;
-//			}
-//
-//			int sizeWithDuplicates = namesOfLines.size();
-//			HashSet<String> linesNoDuplicates = new HashSet<String>();
-//			linesNoDuplicates.addAll(namesOfLines);
-//			int sizeWithoutDuplicates = linesNoDuplicates.size();
-//
-//			if (sizeWithDuplicates != sizeWithoutDuplicates){
-//				Toast.makeText(activity, "Please change duplicate choices", Toast.LENGTH_SHORT).show();
-//				namesValid = false;
-//			}
-//
-//			if (namesValid == true){
-//				
-//				//this means everything worked so should do next step:
-//				
-//				//if it is closed then you are definately the moderator and send create request
-//				SendMessageController.createRequest(Event.getInstance().getIsOpen(), Event.getInstance().getQuestion(), Event.getInstance().getNumChoices(), Event.getInstance().getNumRounds(), Event.getInstance().getUser().getUsername(), Event.getInstance().getUser().getPassword());
-//
-//				//User mod = new User("username", "password", true);					//This was used to test close event controller
-//				//CloseEventView view = new CloseEventView(event, mod, activity);  	//This was used to test close event controller
-//
-//				//CompleteDecisionController results = new CompleteDecisionController(event, activity);	//This was used to test the complete decisions controller
-//
-//			}
-//		}
 
-	
-	//This returns the choices. If any are blank or duplicates, then they are invalid.
-	String[] getChoices(){
+	/**
+	 * This returns the choices. If any are blank or duplicates, then they are invalid.
+	 *
+	 * @return the choices
+	 */
+	public String[] getChoices(){
 		String[] choices;
+
+		// if the event is open
 		if(Event.getInstance().getIsOpen()){
 			choices = new String[1];
-		} else choices = new String[Event.getInstance().getNumChoices()];
+		}
+		//if it is closed, it will check to see if there are any duplicates
+		else choices = new String[Event.getInstance().getNumChoices()];
 		ArrayList<String> linesWithDuplicates = new ArrayList<String>();
 		HashSet<String> linesNoDuplicates = new HashSet<String>();
 		for (int i = 0; i < choices.length; i++){
+
 			String namedLine = textBoxes[i].getText().toString();
-			
 			int choiceNum = i+1;
-			
+
 			linesWithDuplicates.add(namedLine);
 			int sizeWithDuplicates = linesWithDuplicates.size();
 			linesNoDuplicates.addAll(linesWithDuplicates);
@@ -224,8 +170,8 @@ public class AddChoiceController implements android.view.View.OnClickListener{
 				//Toast.makeText(activity, "Choice "+choiceNum+" is a duplicate, please change it", Toast.LENGTH_SHORT).show();
 				namesValid = false;
 			}
-			
-			//check to see if it is a valid edge
+
+			//if there is no dupocates it will then check if there are any blank strings in the textbox
 			if(namesValid && Event.getInstance().checkValidChoice(namedLine)){
 				choices[i] = namedLine;
 			}else{
@@ -240,13 +186,4 @@ public class AddChoiceController implements android.view.View.OnClickListener{
 		}
 		return choices;
 	}
-
-
-
-	public void recieveAddChoiceController(){
-
-
-	}
-
 }
-
