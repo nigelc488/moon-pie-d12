f0152675e29b28a437906fba47c6d6c5ac8a690c
cjb2t488@gmail.com <cjb2t488@gmail.com@78e05d51-7f9b-cf76-7049-e436a2377101>
2012-05-01 02:39:37 +0000

    updating javadocs in my stuff

--- G1MoonPie/src/g1/MoonPie/View/DecisionLinesForm.java
+++ G1MoonPie/src/g1/MoonPie/View/DecisionLinesForm.java
@@ -1,45 +1,60 @@
 package g1.MoonPie.View;
+
 import g1.MoonPie.Controller.AddEdgeController;
 import g1.MoonPie.Model.Edge;
 import g1.MoonPie.Model.Event;
-
 import java.util.ArrayList;
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Align;
 import android.view.View;
+
+
 /**
- * DecisionLinesForm adds a canvas to the application and draws text and lines to model choices, lines, and edges
- * 
- * @author Chris Berthelette
+ * DecisionLinesForm adds a canvas to the application and draws text and lines to model choices, lines, and edges.
  *
+ * @author Chris Berthelette
  */
 public class DecisionLinesForm extends View {
+
+	/** The AddEdgeController. */
 	AddEdgeController addEdge; 
+
+	/** Declares Paint. */
 	Paint paint = new Paint();	
+
+	/** Declares Event. */
 	Event event;
+
+	/** The instance of DecisionLinesForm. */
 	private static DecisionLinesForm instance = null;
+
+	/** The offset from top of screen not including spacing for text. */
 	public int offset;
+
+	/** The context. */
 	static Context context;
+
 	/**
-	 * 
+	 * Instantiates a new decision lines form.
+	 *
 	 * @param context function call for Context.
-	 * @param e function call for Event.
 	 */
 	private DecisionLinesForm(Context context) {
 		super(context);
 		this.event = Event.getInstance();
 		paint.setColor(Color.WHITE);
-		paint.setTextSize(20);
 		paint.setTextAlign(Align.CENTER);
-		
+		paint.setTextSize(20);
 		addEdge = new AddEdgeController(this); 
 	}
+
 	/**
 	 * onDraw sets up the canvas and paints attributes.
+	 *
+	 * @param canvas the canvas
 	 */
 	@Override 
 	public void onDraw(Canvas canvas) {
@@ -53,33 +68,48 @@ public class DecisionLinesForm extends View {
 			int xpos = (i+1)*(width/(event.getNumChoices() +1));
 			canvas.drawText(event.getLines()[i].getChoice(), xpos, offset+40, paint);	
 			canvas.drawLine(xpos, offset+60, xpos, offset + 60 + lineHeight, paint);
-			
+
 		}
 		for (int i = 0; i < event.getEdges().size(); i++) {
-			ArrayList<Edge> edgesList = event.getEdges();
-			Edge thisEdge = edgesList.get(i);
 			event.getEdges().get(i);
-			
+
 			int rescaledLeftLine = addEdge.scaledLeftLine(i);
 			int rescaledRightLine = addEdge.scaledRightLine(i);
 			int unscaledHeight = addEdge.unscaleHeight(i);
-			
+
 			canvas.drawLine(rescaledLeftLine, unscaledHeight, rescaledRightLine, unscaledHeight, paint);
 		}
 	}
+
+	/**
+	 * Gets the single instance of DecisionLinesForm.
+	 *
+	 * @return single instance of DecisionLinesForm
+	 */
 	public static DecisionLinesForm getInstance() {
 		return instance;
 	}
+
+	/**
+	 * Sets the instance.
+	 *
+	 * @param instance the new instance
+	 */
 	public static void setInstance(DecisionLinesForm instance) {
 		DecisionLinesForm.instance = instance;
 	}
 
+	/**
+	 * Sets the context.
+	 *
+	 * @param cont the new context
+	 */
 	public static void setContext(Context cont) {
 		context = cont;
 		instance = new DecisionLinesForm(cont);
 	}
-	
-	
+
+
 
 }
 
